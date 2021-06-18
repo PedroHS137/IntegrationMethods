472e449c483568132203f4c68f38f6a2582aad43
PedroHS137 <pjherreras@hotmail.com>
2021-06-18 00:32:56 -0500

    added comments

--- IntegrationMethods/src/Methods/Integral.java
+++ IntegrationMethods/src/Methods/Integral.java
@@ -4,6 +4,6 @@ public interface Integral {
 	
 	double integrate(double a, double b, int n, Function function);
 	/** la siguente interfaz se encarga de definr el prototipo de la funcion que se implementara 
-	 * en todas las clases que contendran los distintos metodos de integracion */
+	 * en todas las clases que contendran los distintos metodos de integracions */
 	
 }
